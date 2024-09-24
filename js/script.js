0a30cfb23266a3efc99f556984fcc7796478aed4
Darin Berit R S <darinberitrs@gmail.com>
2024-09-24 12:54:37 +0530

    First Commit - Working Simple calculator

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let displayBox = document.getElementById("calc-screen");
+let buttons = document.querySelectorAll("button");
+let expression = "";
+
+let butttonArray = Array.from(buttons);
+butttonArray.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const val = event.target.value;
+        if(val == "all-clear"){
+            expression = "";
+            displayBox.value = expression || 0;
+        }
+        else if(val == "clear"){
+            expression = expression.slice(0,length-1);
+            displayBox.value = expression || 0;
+        }
+        else if(val == "="){
+            expression = eval(expression);
+            displayBox.value = expression;
+        }
+        else{
+            expression += val;
+            displayBox.value = expression;
+        }
+        
+
+    })
+})
